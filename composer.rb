7b3f1c4a115880941c60057610941622f1a59bee
Marlon Moyer <marlon.moyer@acig.com>
2012-12-28 12:11:02 -0500

    prompt user to make sure sqlserver db is created

--- composer.rb
+++ composer.rb
@@ -923,6 +923,7 @@ after_bundler do
     gsub_file "config/database.yml", /database: myapp_development/, "database: #{app_name}_development"
     gsub_file "config/database.yml", /database: myapp_test/,        "database: #{app_name}_test"
     gsub_file "config/database.yml", /database: myapp_production/,  "database: #{app_name}_production"
+    yes_wizard? "Is the database created? (Next steps will fail unless the db exists"
   end
   unless prefer :database, 'sqlite' or prefer :database, 'sqlserver'
     affirm = yes_wizard? "Drop any existing databases named #{app_name}?"
